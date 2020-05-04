402090614f3e752aeafc1c1f3f27729250239ca8
rory.houlihan <rory.houlihan@vistaprint.com>
2020-05-04 14:29:52 +0200

    updated for build and deploy pipeline

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='molecule_parser',
-    url='',
+    url='https://github.com/mcrors/molecule_parser',
     author='Rory Houlihan',
     version=1.0,
     packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'unit_tests']),
