9bb8b5051d8f21ca5fd422d60f66eb154d882888
rory.houlihan <rory.houlihan@vistaprint.com>
2020-05-04 15:19:11 +0200

    remove redundant function

--- molecule_parser/utils.py
+++ molecule_parser/utils.py
@@ -6,9 +6,3 @@ class Utils:
     @staticmethod
     def is_last_item(seq: Sequence, index: int) -> bool:
         return index + 1 >= len(seq)
-
-    @staticmethod
-    def next_item_is_num(seq: Sequence, index: int) -> bool:
-        if not Utils.is_last_item(seq, index):
-            return isinstance(seq[index + 1], int)
-        return False
